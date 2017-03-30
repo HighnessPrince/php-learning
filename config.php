0b0a8f233e38210827bd588fe09da89acee70a95
TalentWang <13616520998@163.com>
2017-03-31 00:32:25 +0800

    Episode 21: Composer autoloading(2)

--- config.php
+++ config.php
@@ -2,9 +2,9 @@
 
 return [
     'database' =>[
-        'name' => 'mytodos',
+        'name' => 'mytodo',
         'username' => 'root',
-        'password' => '',
+        'password' => '666666',
         'connection' => 'mysql:host=127.0.0.1',
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
--- core/bootstrap.php
+++ core/bootstrap.php
@@ -2,11 +2,6 @@
 
 $app = [];
 
-require 'core/database/Connection.php';
-require 'core/database/QueryBuilder.php';
-require 'core/Router.php';
-require 'core/Request.php';
-
 $app['config'] = require 'config.php';
 
 $app['database'] = new QueryBuilder(
